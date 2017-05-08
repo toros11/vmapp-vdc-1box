297ddf69ef7cc89506ee7dc2b6f1c653fe0f2ba4
Tobias Rosen <tobias.rosen@axsh.net>
2017-05-08 21:27:59 +0900

    Added init file for environment

--- ind-steps/step-buildenv/init.sh
+++ ind-steps/step-buildenv/init.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
+
+for dep in "${DEPENDENCIES[@]}" ; do
+    check_dependency "${dep}"
+done
+
+(
+    $starting_step "Environment: download seed image"
+    [ -f "${ORGCODEDIR}/${seed_image}" ]
+    $skip_step_if_already_done
+    curl -o "${ORGCODEDIR}/${seed_image}" -O "https://ci.openvdc.org/img/${seed_image}"
+    $skip_step_if_already_done ; set -ex
+) ; prev_cmd_failed
 
