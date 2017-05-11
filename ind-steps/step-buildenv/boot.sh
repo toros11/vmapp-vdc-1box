dd45f1dd04e821257a3f6c38e566301362ccae27
Tobias Rosen <tobias.rosen@axsh.net>
2017-05-11 18:13:35 +0900

    Remove building in background

--- ind-steps/step-buildenv/boot.sh
+++ ind-steps/step-buildenv/boot.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
 
 (
-    node_build_pid=()
     idx=0
     for node in ${NODES[@]} ; do
         $starting_group "Environment: building ${node}"
         false
         $skip_group_if_unnecessary
-        "${ORGCODEDIR}/nodes/${node}/build.sh" "${idx}" &> "${ORGCODEDIR}/nodes/${node}/build.log" &
+        "${ORGCODEDIR}/nodes/${node}/build.sh" "${idx}" &> "${ORGCODEDIR}/nodes/${node}/build.log"
         idx=$(( idx + 1 ))
         node_build_pid[$!]="${node}"
-    done
-
-    for procc in "${!node_build_pid[@]}" ; do
-        echo "Waiting for ${node_build_pid[procc]}.."
-        wait $procc
     done
 ) ; prev_cmd_failed
